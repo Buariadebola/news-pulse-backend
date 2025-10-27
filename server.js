b81dc112f1db87bc2601c39ee735acbdacb0cd69
unknown <buariadebola@gmail.com>
2025-10-27 22:12:20 +0100

    Add GNews proxy and update CORS settings

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const fs = require('fs');
 require('dotenv').config();
-import axios from 'axios';
+const axios = require('axios');
 
 const app = express();
 app.use(express.json());
